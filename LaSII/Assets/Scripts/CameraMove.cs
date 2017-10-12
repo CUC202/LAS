093975dc2b08178c0f769134301bbc7c725a1e27
Joyce_Meng <JoyceMeng1997@users.noreply.github.com>
2017-10-12 18:38:26 +0800

    增加相机移动效果 增加白的两个技能 10.12

---
+++ LaSII/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+//双人（未合体）模式时的相机移动效果
+public class CameraMove : MonoBehaviour
+{
+    Vector2 campos;
+    public GameObject player1;
+    public GameObject player2;
+    Vector2 p1;
+    Vector2 p2;
+    float x1, y1;
+    float p1w, p1h, p2w, p2h;
+    private Plane[] pan;
+    // Use this for initialization
+    void Start()
+    {
+        p1w = p1h = p2w = p2h = 0.0f;
+    }
+    void Update()
+    {
+        if (player1.GetComponent<SpriteRenderer>() != null && player2.GetComponent<SpriteRenderer>() != null)
+        {
+            p1w = player1.transform.localScale.x * player1.GetComponent<SpriteRenderer>().sprite.bounds.size.x;//获取player1的宽
+            p1h = player1.transform.localScale.y * player1.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+            p2w = player2.transform.localScale.x * player2.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            p2h = player2.transform.localScale.y * player2.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+        }
+        else if (player1.GetComponent<SpriteRenderer>() != null && player2.GetComponent<SpriteRenderer>() == null)
+        {
+            p1w = p1h = 0;
+            p2w = player2.transform.localScale.x * player2.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            p2h = player2.transform.localScale.y * player2.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+        }
+        else if (player2.GetComponent<SpriteRenderer>() != null && player1.GetComponent<SpriteRenderer>() == null)
+        {
+            p2w = p2h = 0;
+            p1w = player1.transform.localScale.x * player1.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+            p1h = player1.transform.localScale.y * player1.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+        }
+        else
+        {
+            p1w = p1h = p2w = p2h = 0;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float width;
+        float height;
+        float leftBorder;
+        float rightBorder;
+        float topBorder;
+        float downBorder;
+        //SetBasicValues ();
+
+        p1 = player1.transform.position;
+        p2 = player2.transform.position;
+        transform.DOMoveX((p1.x + p2.x) / 2, 0.2f);
+        transform.DOMoveY((p1.y + p2.y) / 2, 0.2f);
+        x1 = Mathf.Abs(p1.x - p2.x);
+        y1 = Mathf.Abs(p1.y - p2.y);
+
+        Vector3 cornerPos = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Mathf.Abs(-Camera.main.transform.position.z)));
+
+        leftBorder = Camera.main.transform.position.x - (cornerPos.x - Camera.main.transform.position.x);
+        rightBorder = cornerPos.x;
+        topBorder = cornerPos.y;
+        downBorder = Camera.main.transform.position.y - (cornerPos.y - Camera.main.transform.position.y);
+
+        width = rightBorder - leftBorder;
+        height = topBorder - downBorder;
+        if ((x1 >= (width - 4 * p1w)) && Camera.main.fieldOfView <= 90)
+        {
+            Camera.main.fieldOfView += 25 * Time.deltaTime;
+            if (Camera.main.fieldOfView >= 88)
+            {
+                //启用相机边界碰撞
+            }
+        }
+        if (x1 < width / 2 && y1 < height && Camera.main.fieldOfView >= 35)
+        {
+            Camera.main.fieldOfView -= 25 * Time.deltaTime;
+        }
+        float a = x1 + 4 * p1w - width;
+
+    }
+}
---
+++ LaSII/Assets/Scripts/LP_Skill.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class LP_Skill : MonoBehaviour
+{
+    float lit_sc;
+    public float biggest = 3.0f;//光的最大scale
+    public float litspeed = 2.0f;
+    public float newLightScale = 1.0f;//留下的光的初始scale
+
+    Vector3 lpscale;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lpscale = this.GetComponentInParent<Transform>().localScale;
+
+        LightUp();
+
+        if (Input.GetKeyDown(KeyCode.Q))//测试用，input setting里有LPS2
+        {
+            Debug.Log("S2");
+            SetLight();
+        }
+    }
+    void LightUp()
+    {
+        this.transform.localScale = new Vector3(lit_sc, lit_sc, lit_sc);
+        if (Input.GetKey(KeyCode.Z) && lit_sc > 0f && lit_sc < biggest)//测试用，input setting里有LPS1
+        {
+            //同时损耗能量
+            lit_sc += Time.deltaTime * litspeed;
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            lit_sc = 0.1f;//这里稍微有点矛盾但目前没有出Bug
+            this.transform.DOScale(new Vector3(0.1f, 0.1f, 1f), 0.2f);
+        }
+    }
+    void SetLight()
+    {
+        Vector2 lightpos = this.transform.position;
+        GameObject newl = Instantiate(this.gameObject, lightpos, Quaternion.identity);
+        newl.GetComponent<LP_Skill>().enabled = false;
+        newl.AddComponent<Rigidbody2D>();
+        //newl.transform.localScale = new Vector2(newLightScale, newLightScale);
+
+        //Vector3 finalsc;
+        //finalsc.x = newLightScale * this.transform.localScale.x;
+        //finalsc.y = newLightScale * this.transform.localScale.y;
+
+       // newl.transform.DOScale(new Vector3(this.transform.localScale.x * 1.5f, this.transform.localScale.y * 1.5f, 1), 1.5f);
+        Destroy(newl, 2.0f);
+
+    }
+    void OnColliderEnter2D(Collision2D other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Light":
+                {
+
+                    break;
+                }
+        }
+    }
+}
