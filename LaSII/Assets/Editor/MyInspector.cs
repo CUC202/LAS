0de0e7161e313d9276cdd85466de86b69f8fd02a
Joyce_Meng <JoyceMeng1997@users.noreply.github.com>
2017-10-15 05:20:25 +0800

    几个Editor模板，日后能用到 17.10.15

---
+++ LaSII/Assets/Editor/MyInspector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class MyScript : MonoBehaviour
+{
+    public bool flag;
+    public int i = 1;
+}
+public class MyInspector : Editor
+{
+    [CustomEditor(typeof(MyScript))]
+
+    void OnInspectorGUI()
+    {
+        var myScript = target as MyScript;
+
+        myScript.flag = GUILayout.Toggle(myScript.flag, "Flag");
+
+        if (myScript.flag)
+            myScript.i = EditorGUILayout.IntSlider("I field:", myScript.i, 1, 100);
+
+    }
+}
