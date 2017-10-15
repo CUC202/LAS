8103d47a797c93da3b734508294c3f0f292bb86d
Joyce_Meng <JoyceMeng1997@users.noreply.github.com>
2017-10-15 23:25:22 +0800

    闪烁先给做了 旋转的脚本可以直接拖darkness的用 没什么事的话先搭场景了 17.10.15

--- LaSII/Assets/MyScripts/Lightings.cs
+++ LaSII/Assets/MyScripts/Lightings.cs
@@ -6,31 +6,50 @@ using DG.Tweening;
 //之后的两个悬挂点光源，会弧形摆动（以悬挂点为圆心旋转）
 //之后会在editor里做详细的规划
 public class Lightings : MonoBehaviour {
-    public float size;
-    [Tooltip("0=点形  1=柱形  2=扇形 3=能量")]public int type;
+    public float size=1.0f;
+    [Tooltip("1=点形  2=柱形  3=扇形 4=能量")]public int type;
 
     public bool is_Rotate;
     public bool is_Moving;
+    public bool is_Flashing;
     public Vector2 startp;
     public Vector2 endp;
     public float speed;
     public float pauseTime;
+    [Range(0,255)]public int 透明度=100;
+    Vector3 nowsc;
     // Use this for initialization
-    void Start () {
+
+    int colortrans;
+    void Awake()
+    {
+        colortrans = 255 - 透明度;
         transform.position = new Vector3(startp.x, startp.y, 0);
-	}
+        this.transform.localScale = new Vector3(size, size, 1);
+        nowsc = this.transform.localScale*size;
+    }
+    void Start () {
+        move();
+     }
 	
 	// Update is called once per frame
 	void Update () {
+        //move();
     }
     void move()
     {
         switch (type)
         {
-            case 0: fly(); break;
-            case 1: break;
+            case 1:
+                {
+                    if (is_Moving) fly();
+                    if (is_Rotate) rot();
+                    if (is_Flashing) flash();
+                }
+                break;
             case 2: break;
             case 3: break;
+            case 4: break;
             default: break;
         }
     }
@@ -39,14 +58,43 @@ public class Lightings : MonoBehaviour {
         transform.DOLocalMove(new Vector3(endp.x, endp.y, 0), 2.0f, false);
         Invoke("stay", 2.0f);
     }
+    void flyback()
+    {
+        transform.DOLocalMove(new Vector3(startp.x, startp.y, 0), 2.0f, false);
+        Invoke("stay", 2.0f);
+    }
+    /// <summary>
+    /// ///////////////////////
+    /// </summary>
     void rot()//柱形旋转
     {
 
     }
+    /// <summary>
+    /// //////////////
+    /// </summary>
     void sca()//扇形扩大和缩小
     {
 
     }
+    /// <summary>
+    /// /////////////////
+    /// </summary>
+    void flash()//不动闪烁
+    {
+        this.GetComponent<Renderer>().material.DOBlendableColor(new UnityEngine.Color((float)colortrans / 255, (float)colortrans / 255, (float)colortrans / 255), 1.0f);
+        this.transform.DOScale(nowsc * 1.2f, 1.0f);
+        Invoke("flash2", 1.0f);
+    }
+    void flash2()
+    {
+        this.GetComponent<Renderer>().material.DOBlendableColor(Color.white, 1.0f);
+        this.transform.DOScale(nowsc, 1.0f);
+        Invoke("flash", 1.0f);
+    }
+    /// <summary>
+    /// /////////////////////////
+    /// </summary>
     void stay()
     {
         switch (type)
@@ -59,9 +107,5 @@ public class Lightings : MonoBehaviour {
         }
        
     }
-    void flyback()
-    {
-        transform.DOLocalMove(new Vector3(startp.x, startp.y, 0), 2.0f, false);
-        Invoke("stay", 2.0f);
-    }
+    
 }
