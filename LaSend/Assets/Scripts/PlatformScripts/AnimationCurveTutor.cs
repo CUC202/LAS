88e64fec644e6892824ec537eba4c226586963a1
Joyce_Meng <JoyceMeng1997@users.noreply.github.com>
2018-03-14 15:53:01 +0800

    重新开始end

---
+++ LaSend/Assets/Scripts/PlatformScripts/AnimationCurveTutor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationCurveTutor : MonoBehaviour
+{
+    public AnimationCurve move_anim;
+    public AnimationCurve rot_anim;
+    public bool is_move;
+    public bool is_rotate;
+    
+    void update()
+    {
+        if(is_move)
+        {
+            transform.position = new Vector3(Time.time, move_anim.Evaluate(Time.time), 0);
+        }
+        if(is_rotate)
+        {
+            transform.localEulerAngles = new Vector3(0,0,rot_anim.Evaluate(Time.time));
+        }
+    }
+}
---
+++ LaSend/Assets/Scripts/PlayerScripts/testscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Physics.gravity = new Vector3(-Physics.gravity.y, Physics.gravity.x, 0);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.z + 90));
+            Debug.Log(Physics.gravity.y);
+            Debug.Log(Physics.gravity.x);
+        }
+    }
+}
